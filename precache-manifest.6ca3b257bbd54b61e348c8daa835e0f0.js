1f1e1f7f801faec6915eb90748938324f71eeb30
Roman Nguyen <march08@outlook.com>
2019-01-30 11:51:55 +0100

    Updates

--- precache-manifest.6ca3b257bbd54b61e348c8daa835e0f0.js
+++ precache-manifest.6ca3b257bbd54b61e348c8daa835e0f0.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/animated-burgers/static/js/runtime~main.7c6e612f.js"
   },
   {
-    "revision": "4338346362e0294be068",
-    "url": "/animated-burgers/static/js/main.43383463.chunk.js"
+    "revision": "f83f4fb31625cd778740",
+    "url": "/animated-burgers/static/js/main.f83f4fb3.chunk.js"
   },
   {
     "revision": "2ee560e3e59abff8920e",
     "url": "/animated-burgers/static/js/1.2ee560e3.chunk.js"
   },
   {
-    "revision": "4338346362e0294be068",
+    "revision": "f83f4fb31625cd778740",
     "url": "/animated-burgers/static/css/main.d4b25546.chunk.css"
   },
   {
-    "revision": "c4e3569bdccb87024cb496ab81f20fd9",
+    "revision": "63f6f6655cfb4d309e0f6fd24dbe5da2",
     "url": "/animated-burgers/index.html"
   }
 ];
