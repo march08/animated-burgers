7c1038cfd92a11986544b2e218f9144ac52b1632
Roman Nguyen <march08@outlook.com>
2019-01-30 01:19:34 +0100

    Updates

--- precache-manifest.ee639f8f5e30093cc877ac91cf167ecf.js
+++ precache-manifest.ee639f8f5e30093cc877ac91cf167ecf.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/animated-burgers/static/js/runtime~main.7c6e612f.js"
   },
   {
-    "revision": "fc9d380ab04caa2a0e0e",
-    "url": "/animated-burgers/static/js/main.fc9d380a.chunk.js"
+    "revision": "91b2db6bcb37eff93133",
+    "url": "/animated-burgers/static/js/main.91b2db6b.chunk.js"
   },
   {
     "revision": "2ee560e3e59abff8920e",
     "url": "/animated-burgers/static/js/1.2ee560e3.chunk.js"
   },
   {
-    "revision": "fc9d380ab04caa2a0e0e",
+    "revision": "91b2db6bcb37eff93133",
     "url": "/animated-burgers/static/css/main.d4b25546.chunk.css"
   },
   {
-    "revision": "03f90f580105bab9fbdd81109c1784b6",
+    "revision": "8206cdff5dda2e56375691b8294d5b45",
     "url": "/animated-burgers/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/animated-burgers/precache-manifest.3e57789df2b96eb59a4ac20644c60653.js"
+  "/animated-burgers/precache-manifest.ee639f8f5e30093cc877ac91cf167ecf.js"
 );
 
 workbox.clientsClaim();
